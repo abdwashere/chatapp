307f1b59ab74aa40596749c731121736de9f59ab
abdwashere <abdurrehmankhan32343@gmail.com>
2026-06-04 01:51:47 +0500

    bind to 0.0.0.0

--- server.js
+++ server.js
@@ -431,4 +431,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Pulse Chat → http://localhost:${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Pulse Chat → http://localhost:${PORT}`));
